0b47eb5a8b7e8ddfaa8acdba44ab1f3e89436cd5
Emre Koca <ekoca1@gmail.com>
2015-02-18 17:39:25 -0800

    Checked it in

--- app.js
+++ app.js
@@ -3,3 +3,8 @@ var express = require('express');
 var app = express();
 app.use(express.static(__dirname + '/app'));
 app.listen(process.env.PORT || 3000);
+
+// Using a catch all route middleware in my node/Express server as follows (put it after the router)
+app.use(function(req, res) {
+    res.sendfile(__dirname + '/app/index.html');
+});
