f128fc9690ea5a93ae6bfc5e952901d39da71f03
Emre Koca <ekoca1@gmail.com>
2015-02-20 17:27:57 -0800

    fixing https issue

--- app.js
+++ app.js
@@ -8,3 +8,16 @@ app.listen(process.env.PORT || 3000);
 app.use(function(req, res) {
     res.sendfile(__dirname + '/app/index.html');
 });
+
+app.use(function(req, res, next) {
+  var schema = req.headers['x-forwarded-proto'];
+
+  if (schema === 'http') {
+    // Already https; don't do anything special.
+    next();
+  }
+  else {
+    // Redirect to https.
+    res.redirect('http://' + req.headers.host + req.url);
+  }
+});
